a2cb47439d003bfae1287c426054f7f5cb0d61d9
fixuberi <comeclarity007@gmail.com>
2019-02-20 00:43:32 +0200

    Compan info layout in progres

--- src/components/CompanyInfo.js
+++ src/components/CompanyInfo.js
@@ -1,25 +1,60 @@
 import React from 'react';
 
 const CompanyInfo = ({ data, isFetching }) => {
-    const infoElements = [];
-    for (let key in data) {
-        infoElements.push({ key: key, value: data[key] })
+    const { copanyName, 
+            description,
+            tags } = data;
+    const basicInfo = () => {
+        return ['industry', 'sector', 'CEO', 'exchange'].map(infoKey => {
+            if(!data[infoKey]) return '';
+            return(
+                <BasicInfoElemment>
+                    <div className='info-key'>{infoKey}</div>
+                    <div className='info-value'>{data[infoKey]}</div>
+                </BasicInfoElemment>
+            );
+        }) 
+    }
+    const website = () => {
+        const website = data['website'];
+        if(data[website]) return '';
+        return(
+            <BasicInfoElemment>
+                <div className='info-key'>Website</div>
+                <div className='info-value'>
+                    <a href={website} />
+                </div>
+            </BasicInfoElemment>
+        );
+    }
+    const description = () => {
+        const description = data['description'];
+        if(data[description]) return(
+            <Description>
+                <h3>Description</h3>
+                <p>{description}</p>
+            </Description>
+        );
     }
     return (
-        <ul>
-            {infoElements.map(el => {
-                return <li key={el.key}>
-                            {`${el.key} - ${el.value}`}
-                        </li>
-            })}
-        </ul>
         <CompanyInfoWrapper>
             <MainInfo>
-                <CompanyName></CompanyName>
-                {/* industry sector ceo exchange web */}
+                <CompanyName>
+                    <h2>
+                        {copanyName}
+                    </h2>
+                </CompanyName>
+                <BasicInfo>
+                    {basicInfo()}
+                    {website()}
+                </BasicInfo>
             </MainInfo>
-            <Description></Description>
-            <Tags></Tags>
+            <DescriptionWrapper>
+                {description()}
+            </DescriptionWrapper>
+            <Tags>
+                {tags}
+            </Tags>
         </CompanyInfoWrapper> 
     )
 }
