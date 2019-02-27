371a6294ae68f5c8d7d80b0339a4c9e19e3a8357
fixuberi <comeclarity007@gmail.com>
2019-02-27 12:32:59 +0200

    Conditional route for Charts page

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,30 @@ import NotFound from '../components/NotFound';
 import { RegistrationContainer } from '../containers/RegistrationContainer';
 import { LoginContainer } from '../containers/LoginContainer';
 
+const User = require('oauthio-web').User;
+
+class ConditionalChartsRoute extends React.Component {
+    render() {
+        const { component: Component, ...props } = this.props;
+        return(
+            <Route 
+                {...props}
+                render={ props => (
+                    User.isLogged() ? 
+                    <ChartsContainer isLogged={true} {...props} /> : 
+                    <ChartsContainer isLogged={false} {...props} />
+                )}
+            />
+        );
+    }
+}
+
 export default (
         <Switch>            
             <Route exact path="/" component={Home}/>
-            <Route exact path="/search/" component={ChartsContainer}/>
             <Route exact path="/signup" component={RegistrationContainer}/>
             <Route exact path="/signin" component={LoginContainer}/>
-            <Route exact path="/search/:symbol" component={ChartsContainer}/>
+            <ConditionalChartsRoute exact path="/search/:symbol?" component={ChartsContainer} />
             <Route component={NotFound}/>
         </Switch>
     )
